fd72b1470a9d6e8d6aa6c1ff784252b8e2c65672
Alan <sevilbm94@hotmail.com>
2019-08-25 18:07:03 -0500

    Interfaz header

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const expressHbs = require('express-handlebars');
+
 const globalErrorHandler = require('./controllers/errorController');
 const productoRouter = require('./routes/productoRouter');
 const usuarioRouter = require('./routes/usuarioRouter');
 const carritoRouter = require('./routes/carritoRouter');
 
 const app = express();
+
+// app.engine(expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', 'hbs');
 
 // Este middleware pertmite que se puedan leer argumentos del req.body
 app.use(express.json({ limit: '10kb' }));
