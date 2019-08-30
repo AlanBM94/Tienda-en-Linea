ecd61fe49e0488d184f634b38544f95151c9016b
Alan <sevilbm94@hotmail.com>
2019-08-29 23:31:34 -0500

    Navegacion sticky y pequeñas animaciones

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  // Sticky navigation
+  $('.caracteristicas').waypoint(
+    function(direction) {
+      if (direction == 'down') {
+        $('.nav').addClass('sticky');
+        $('.nav__lista').css({ background: '#232121' });
+      } else {
+        $('.nav').removeClass('sticky');
+        $('.nav__lista').css({ background: 'transparent' });
+      }
+    },
+    {
+      offset: '80px;'
+    }
+  );
+  //Animaciones de las secciones
+  $('.caracteristicas').waypoint(
+    function(direction) {
+      $('.caracteristicas__contenido').css({ opacity: '1' });
+    },
+    {
+      offset: '10%'
+    }
+  );
+
+  $('.testimonial').waypoint(
+    function(direction) {
+      $('.testimonial__contenedorGrande').css({ opacity: '1' });
+    },
+    {
+      offset: '10%'
+    }
+  );
+});
